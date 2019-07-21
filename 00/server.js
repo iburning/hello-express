d53a8d533fde8a0ce74c12fc924aa169e0ecd132
burning <iburning@live.cn>
2019-07-21 21:10:34 +0800

    handle request url

--- 00/server.js
+++ 00/server.js
@@ -1,9 +1,30 @@
 const http = require('http')
+const querystring = require('querystring')
 
 const server = http.createServer()
 
 server.on('request', (req, res) => {
-  res.end('hello world')
+  const [url, query_string] = req.url.split('?')
+  const query = querystring.parse(query_string)
+
+  console.log(req.url, url, query)
+  switch (url) {
+    case '/':
+      res.end('<h1>Page Home</h1><p>hello world</p>')
+      break
+
+    case '/blog':
+      const id = query.id
+      res.end(`<h1>Page Blog</h1><p>bolg id = ${id}</p>`)
+      break
+
+    case '/about':
+      res.end('<h1>Page About</h1>')
+      break
+
+    default:
+    res.end('<h1>Page Not Found</h1>')
+  }
 })
 
 server.on('error', err => {
